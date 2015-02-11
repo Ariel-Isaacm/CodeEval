e682a1dca76da998b2cf67075c373be016b20c2d
Ariel-Isaacm <ariel.isaacm@gmail.com>
2015-02-11 13:29:03 -0700

    Unique Elements

---
+++ Easy/UniqueElements.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System;
+using System.Collections;
+
+namespace UniqueElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            string[] numeros;
+            ArrayList final;
+            string salida;
+            using (StreamReader reader = File.OpenText("C:/Users/Admin/Desktop/texto.txt"))
+            
+                while (!reader.EndOfStream)
+                {
+                    salida = null;
+                    final = new ArrayList();
+                    line = reader.ReadLine();
+                    numeros=line.Split(',');
+                    
+                    foreach (var item in numeros)
+                    {
+                        if (!final.Contains(item))
+                        {
+                            final.Add(item);
+                        }
+                    }
+                    foreach (var item in final)
+                    {
+                        salida += item+",";
+                        
+                    }
+
+                    Console.WriteLine(salida.Substring(0,salida.Length-1));
+                }
+            Console.ReadKey();
+            
+        }
+    }
+}
